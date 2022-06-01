7f9c63b60eccd2ba24cd87913ee464cf29904d0f
NataliaIv90 <ivantsovana@gmail.com>
2022-06-01 16:45:51 +0300

    Events hw

---
+++ js/events.js
@@ -0,0 +1,18 @@
+// Challenge 1
+// Add click eventListener to the button. Alert `It is 18 degrees`
+
+function alertWeather() {
+  alert(`It is 18 degrees`);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", alertWeather);
+
+// Challenge 2
+// When the button is clicked, replace button label by ‘18 degrees’
+
+function changeButton() {
+  button.innerHTML = "18 degrees";
+}
+
+button.addEventListener("click", changeButton);
